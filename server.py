c3ce211e2241c359be798cee3e530f331e67b690
Leon Schröder <schreon.loeder@gmail.com>
2014-01-13 23:01:38 +0100

    fixing WAIT - MATCH_FOUND bug

--- server.py
+++ server.py
@@ -32,7 +32,7 @@ app.json_encoder = MongoEncoder
 import logging
 from logging.handlers import RotatingFileHandler
 file_handler = RotatingFileHandler(os.environ['BOCK_LOG'], maxBytes=1024 * 1024 * 100, backupCount=20)
-file_handler.setLevel(logging.ERROR)
+file_handler.setLevel(logging.INFO)
 formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 file_handler.setFormatter(formatter)
 app.logger.addHandler(file_handler)

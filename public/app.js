28a7a1bb09c1323617b2844ce70ea1db9bf9ec41
Leon Schröder <schreon.loeder@gmail.com>
2014-01-13 21:56:17 +0100

    status polls

--- public/app.js
+++ public/app.js
@@ -1,5 +1,21 @@
-var app = angular.module('serverapp', ['ui.bootstrap']);
+'use strict';
 
-function StatusConroller($scope) {
-	
-}
+var app = angular.module('serverapp', ['ui.bootstrap', 'statusService']);
+
+angular.module('statusService', ['ngResource']).
+    factory('Data', function ($resource) {
+        return $resource('status', {}, {
+            query: { method: 'GET', params: {}, isArray: false }
+        });
+    });
+
+function StatusConroller($scope, $timeout, Data) {
+	$scope.data = [];
+	$scope.buffer = [];
+    (function tick() {
+        $scope.buffer = Data.query(function(){
+        	$scope.data = $scope.buffer;
+            $timeout(tick, 1000);            
+        });
+    })();
+};

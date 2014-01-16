9de0e9ab32756ce5d4c9304663a95f0c847ecafc
Leon Schröder <schreon.loeder@gmail.com>
2014-01-16 10:50:56 +0100

    encoding problems

--- server.py
+++ server.py
@@ -76,7 +76,7 @@ def sanitize_loc(loc):
 
 def sanitize_tag(tag):
 
-    return tag.lower().encode('utf-8')
+    return tag.decode('utf-8').lower().encode('utf-8')
 
 @app.route("/")
 def get_index():
